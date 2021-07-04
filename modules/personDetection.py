675d30604a1fcef7dd27123563c03dd891aab578
Prajwal-P <prajwal72p@gmail.com>
2021-07-04 14:00:37 +0530

    added support for video

--- modules/personDetection.py
+++ modules/personDetection.py
@@ -1,8 +1,9 @@
 # import the necessary packages
-from .config import NMS_THRESHOLD, DETECTION_THRESHOLD, PEOPLE_COUNTER, PATH
+from .config import NMS_THRESHOLD, DETECTION_THRESHOLD, PEOPLE_COUNTER, PATH, CAMERA_NO
 import numpy as np
 import cv2
 import time
+import argparse
 import os
 
 
@@ -71,7 +72,7 @@ def detect_people(frame, net, ln, personIdx=0):
     if PEOPLE_COUNTER:
         human_count = "Human count: {}".format(len(idxs))
         cv2.putText(frame, human_count,
-                    (frame.shape[0] - 170, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
+                    (frame.shape[0] - 150, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
 
     # ensure at least one detection exists
     if len(idxs) > 0:
@@ -91,6 +92,26 @@ def detect_people(frame, net, ln, personIdx=0):
     return results
 
 
+def drawBBox(result, image):
+    # loop over the results
+    for (i, (prob, bbox, centroid)) in enumerate(result):
+        # extract the bounding box
+        (startX, startY, endX, endY) = bbox
+
+        # Draw:
+        # (1) a bounding box around the person,
+        # (2) write confidence score and
+        # (3) centroid coordinates of the person
+        cv2.rectangle(image, (startX, startY),
+                      (endX, endY), (0, 255, 255), 2)
+        confidence = '{: .2f}%'.format(prob * 100)
+        cv2.putText(image, confidence, (startX, startY - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
+        cv2.circle(image, centroid, 1, (0, 0, 255), 2)
+
+    return image
+
+
 if __name__ == "__main__":
     LABELS = open(PATH['YOLO_LABELS']).read().strip().split("\n")
     net = cv2.dnn.readNetFromDarknet(PATH['YOLO_CONFIG'], PATH['YOLO_WEIGHTS'])
@@ -98,36 +119,54 @@ if __name__ == "__main__":
     ln = net.getLayerNames()
     ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
-    # # sample image path
-    # imgpath = './sample_dataset/1.jpg'
-    # loop to read one image at a time
-    for path in os.listdir(PATH['SAMPLE_DATASET']):
-        imgpath = os.path.join(PATH['SAMPLE_DATASET'], path)
-        image = cv2.imread(imgpath)
-        image = cv2.resize(image, (720, 640))
+    ap = argparse.ArgumentParser()
+    ap.add_argument('-v',
+                    '--video',
+                    nargs='?',
+                    const=CAMERA_NO,
+                    type=str,
+                    help='Path for the video input. If no path is provided primary web camera will be used for video input')
+    args = vars(ap.parse_args())
 
-        results = detect_people(
-            image, net, ln, personIdx=LABELS.index("person"))
+    if args['video'] is not None:
+        video = cv2.VideoCapture(args['video'])
+        while video.isOpened():
+            ret, image = video.read()
+            if image is None:
+                break
+            width = 720
+            (h, w) = image.shape[:2]
+            r = width / float(w)
+            d = (width, int(h * r))
+            image = cv2.resize(image, d)
+            if ret == False:
+                break
+            results = detect_people(
+                image, net, ln, personIdx=LABELS.index("person"))
+            resImage = drawBBox(results, image.copy())
+            # cv2.imshow('Window-1', image)
+            cv2.imshow('Window-2', resImage)
+            key = cv2.waitKey(1)
+            if key == 27:  # if ESC is pressed, exit loop
+                cv2.destroyAllWindows()
+                break
+        video.release()
+    else:
+        # # sample image path
+        # imgpath = './sample_dataset/1.jpg'
+        # loop to read one image at a time
+        for path in os.listdir(PATH['SAMPLE_DATASET']):
+            imgpath = os.path.join(PATH['SAMPLE_DATASET'], path)
+            image = cv2.imread(imgpath)
+            image = cv2.resize(image, (720, 640))
 
-        # loop over the results
-        for (i, (prob, bbox, centroid)) in enumerate(results):
-            # extract the bounding box
-            (startX, startY, endX, endY) = bbox
+            results = detect_people(
+                image, net, ln, personIdx=LABELS.index("person"))
+            resImage = drawBBox(results, image.copy())
+            cv2.imshow('image', image)
 
-            # Draw:
-            # (1) a bounding box around the person,
-            # (2) write confidence score and
-            # (3) centroid coordinates of the person
-            cv2.rectangle(image, (startX, startY),
-                          (endX, endY), (0, 255, 255), 2)
-            confidence = '{: .2f}%'.format(prob * 100)
-            cv2.putText(image, confidence, (startX, startY - 10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
-            cv2.circle(image, centroid, 1, (0, 0, 255), 2)
-        cv2.imshow('image', image)
-
-        # pauses for 2 seconds before fetching next image
-        key = cv2.waitKey(2000)
-        if key == 27:  # if ESC is pressed, exit loop
-            cv2.destroyAllWindows()
-            break
+            # pauses for 2 seconds before fetching next image
+            key = cv2.waitKey(2000)
+            if key == 27:  # if ESC is pressed, exit loop
+                cv2.destroyAllWindows()
+                break
